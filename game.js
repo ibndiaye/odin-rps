4db3f1869dd3703327e085c0206e608ec4e09871
ibndiaye <63436162+ibndiaye@users.noreply.github.com>
2024-05-26 17:31:48 +0200

    Add playRound function
    Algorithm checks who won and increments the corresponding variable

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+let ties = humanScore = computerScore = 0;
+
 function getComputerChoice() {
     let weapon = ["rock", "paper", "scissors"];
     return weapon[Math.floor(Math.random() * weapon.length)];
@@ -5,5 +7,27 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let weapon = prompt("Make your choice (rock, paper, scissors):");
-    return weapon;
+    return weapon.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    let computerWon, humanWon;
+    if ((humanChoice == "rock" && computerChoice == "scissors")
+        || (humanChoice == "paper" && computerChoice == "rock")
+        || (humanChoice == "scissors" && computerChoice == "paper")) {
+        humanWon++;
+        console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+    } else if ((computerChoice == "rock" && humanChoice == "scissors")
+        || (computerChoice == "paper" && humanChoice == "rock")
+        || (computerChoice == "scissors" && humanChoice == "paper")) {
+        computerWon++;
+        console.log(`You loose, ${computerChoice} beats ${humanChoice}`);
+    } else if (humanChoice == computerChoice) {
+        ties++;
+    }
+}
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice)
