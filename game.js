8e4b535270216900606f212c4a660af94212133f
ibndiaye <63436162+ibndiaye@users.noreply.github.com>
2024-05-26 17:39:40 +0200

    Implement for loop and changes
    alert the status of the game.
    the for loop loops 5 times, so that the games re-runs 5 times

--- game.js
+++ game.js
@@ -11,23 +11,30 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-    let computerWon, humanWon;
+    let computerWon, humanWon, result;
     if ((humanChoice == "rock" && computerChoice == "scissors")
         || (humanChoice == "paper" && computerChoice == "rock")
         || (humanChoice == "scissors" && computerChoice == "paper")) {
         humanWon++;
-        console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+        result=`You win, ${humanChoice} beats ${computerChoice}`;
     } else if ((computerChoice == "rock" && humanChoice == "scissors")
         || (computerChoice == "paper" && humanChoice == "rock")
         || (computerChoice == "scissors" && humanChoice == "paper")) {
         computerWon++;
-        console.log(`You loose, ${computerChoice} beats ${humanChoice}`);
+        result=`You loose, ${computerChoice} beats ${humanChoice}`;
     } else if (humanChoice == computerChoice) {
         ties++;
+        result=`It's a tie, ${humanChoice}=${computerChoice}`;
+    }
+    alert(result);
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
     }
 }
 
-humanChoice = getHumanChoice();
-computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice)
+playGame();
